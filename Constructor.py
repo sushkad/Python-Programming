8b3c2292ac792cf2584332e43051e6a4690126c8
sushkad <sushant.kadam27@gmail.com>
2025-02-01 23:41:47 +0530

    Constructor and Evenodd

---
+++ Constructor.py
@@ -0,0 +1,13 @@
+
+class Calculator:
+    num = 100
+
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+    def getData(self):
+        print("Execute method in class")
+
+obj = Calculator()
+obj.getData()
+print(obj.num)
