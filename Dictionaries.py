d14bc054e08d392036c6f75df23d08544189f90c
sushkad <sushant.kadam27@gmail.com>
2025-02-08 00:22:03 +0530

    Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,10 @@
+
+# store name value pairs or key value paiars
+
+movie = {
+    'title' : 'Life of Brian',
+    'year' : 1979,
+    'cast' : ['John','Eric','Michael','George','Terry']
+}
+print(movie)
+
