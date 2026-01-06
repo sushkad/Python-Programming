2202daf48706b773250e545319116d44f6b49bf8
sushantkadam <sushant.kadam27@gmail.com>
2026-01-06 22:27:50 +0530

    updated code data types

--- Strings.py
+++ Strings.py
@@ -14,3 +14,17 @@ print(str+str1)         # Concatinate
 print(str3 in str)      # False str3 is present in str or not
 
 print(str.split("."))
+
+print('---------------------------------------------------------------------------------------------')
+
+
+print("start python pgm")
+
+age  = 27
+name  = "sushant"
+
+height = 6.1
+print(age, name , height)
+
+
+
