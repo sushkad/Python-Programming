7d739dc00d19f69efcd69c16522853333f834bbf
sushantkadam <sushant.kadam27@gmail.com>
2026-01-23 22:35:23 +0530

    Calculator and Greetuser

---
+++ Greetfuction.py
@@ -0,0 +1,11 @@
+
+#Write a function called GreetUser that takes a single argument username.
+#The function should print "Hello, [username]! Welcome to the Python course."
+#Call the function with username "John".
+
+
+def GreetUser(username):
+    print(f"Hello,{username}! Welcomr to the Python Course.")
+
+
+GreetUser("John")
