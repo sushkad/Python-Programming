2bffed5ad1a187a5bed4b3321d7a11b50be2b5f4
sushantkadam <sushant.kadam27@gmail.com>
2026-02-08 20:45:02 +0530

    Concatenate two strings and Store person details

---
+++ Concatenate two strings and print the result.py
@@ -0,0 +1,10 @@
+
+
+
+string1 = input("Enter first string: ")
+string2 = input("Enter second string: ")
+
+
+result = string1 + string2
+
+print(result)
---
+++ storing a person's details.py
@@ -0,0 +1,8 @@
+
+
+name = "sushant kadam"
+age = 35
+average_test_score = 89.5
+
+
+print(f"Name: {name}, Age: {age}, Average test score: {average_test_score}")
