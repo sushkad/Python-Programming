fbc5d1633b1c4b48efe7b7104fac36b6c046b125
sushantkadam <sushant.kadam27@gmail.com>
2026-02-21 22:26:01 +0530

    2pgm

---
+++ print first and last number.py
@@ -0,0 +1,15 @@
+def access_elements(my_list):
+    # Access and print the first element
+    print(my_list[0])
+
+    # Access and print the last element
+    print(my_list[4])
+
+
+# Example list
+example_list = [10, 20, 30, 40, 50]
+access_elements(example_list)
+
+# Expected Output:
+# 10
+# 50
