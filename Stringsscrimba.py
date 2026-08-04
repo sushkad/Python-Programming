3d47a19d4e0ee44973bdfa18987a069739b86984
sushantkadam <sushant.kadam27@gmail.com>
2026-08-04 17:03:46 +0530

    StringsScrimba

---
+++ Stringsscrimba.py
@@ -0,0 +1,29 @@
+
+
+
+breakfast = "Pancake"
+lunch =  "salad"
+dinner = "pizza"
+
+meal_plan = " Today i will eat: " +  breakfast +","  + lunch + "," + dinner
+print(meal_plan)
+
+
+# Challenge - Daily Planner
+# Print a sentence that outlines your plan for today.
+
+# 1. Make three variables: morning, afternoon, and evening.
+# 2. To each variable, save a string containing one activity (taking a nap, skiing, etc).
+# 3. Add each variable together and save to a variable called `plan_for_today`
+
+# Here's an example of what your output should look like:
+# "My plan for today day is: 1. drink coffee 2. study Python 3. dance party"
+# Hint: There's no magic to numbering the list, just include them in your strings!
+
+
+morning =  "drink milk"
+afternoon =  "drink buttermilk"
+evening =  "drink soda"
+
+plan_for_today = "My Plan for today day is: " + morning +"," + afternoon + " and " + evening
+print(plan_for_today)
