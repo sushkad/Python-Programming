755aa0104a0ee45e3324637fa9e9e0c23c0f9268
sushantkadam <sushant.kadam27@gmail.com>
2026-08-06 23:55:24 +0530

    datatypes scrimba

---
+++ datatypesqa.py
@@ -0,0 +1,20 @@
+
+
+print(-13%3)
+
+
+print(0.1 + 0.2 )
+
+
+price = 30
+quantity = 5
+
+print(f"The total price is: {price * quantity}")
+
+name = input("Enter Your name:")
+
+print(f"Hello, {name}!")
+
+
+nums =[2,3,5,0,1]
+print(f"The maximum number is: {max(nums)}")
