283e12b69bee053c1e1f30028ba3734dbed1b0c3
sushantkadam <sushant.kadam27@gmail.com>
2026-08-08 23:50:44 +0530

    list and tuples exe

--- How to Find the Largest Number in a Python List.py
+++ How to Find the Largest Number in a Python List.py
@@ -1,9 +1,39 @@
 
 
-# How to Find the Largest & lowest Number in a Python List
-list = [ 2,5,99,100,200]
+#List
+numbers = [2,5,99,100,200]
+names =['sushant','kadam']
 
-print(max(list))
+print(max(numbers))
+print('----------------')
+print(min(numbers))
+print('----------------')
+
+print(type(numbers))
+print('----------------')
+print(type(names))
 
 
-print(min(list))
+#Tuples
+
+team = (100,1001,2001,3001)
+manager = "Team India"
+
+print(type(team))
+print(manager)
+
+
+print('###############################')
+
+man = "sushant"
+age = 36
+salary = 15.30
+
+print(type(man))
+print(type(age))
+print(type(salary))
+
+
+print('00000000')
+t = (66,67)
+print(type(t))
